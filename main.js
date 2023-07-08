c7957d1f5d5c347533163b2ce097fe5f835a8b00
Aditeyo <106545578+Aditeyo@users.noreply.github.com>
2023-07-08 12:23:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var song="";
+
+function preload(){
+loadSound("pandora-140533.mp3");
+loadSound("password-infinity-123276.mp3");
+loadSound("epic-cinematic-beautiful-dubstep-141372.mp3");
+}
+
+function setup(){
+canvas=createCanvas(400,200);
+canvas.center();
+image(video,20,-30,400,200);
+}
+
+function draw(){
+    
+}
